af0461ca14f2c9449c1c5b8e4f1e01e5afb6def0
netdev98 <netdev98@gmail.com>
2026-04-15 15:58:25 +0800

    Add project files.

---
+++ Exam/Model/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Exam.Model
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        [Range(18, 120, ErrorMessage = "Must be an adult")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
+    }
+
+}
